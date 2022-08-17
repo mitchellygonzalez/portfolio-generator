b2c64a2d4e1e1d0c30a0594a46a4fe3371cf1c52
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-08-16 23:21:30 -0400

    done here

--- app.js
+++ app.js
@@ -1,87 +1,139 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-inquirer
-  .prompt([
+const generatePage = require('./page-template');
+
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
-  ])
-
-
-  .then(answers => console.log(answers));
-
-
-/// const fs = require('fs');
-/* ^ to use the fs module, add this to the top of the JS file.
- The require statement is a built-in function that's globally avaiable in Node.js. 
- It allows the app.js file to access the fs module's function through the fs assignment */
-/// const generatePage = require('./page-template');
-/* we're adding this above b/c we put the requre at the top of the page of the 
-destination file(this one) that we want to receive exported functions to (receive from the source file -- page-template.js)
-*/
-// NOTE: the variable name is arbitrary; however the reltive path to include the file must be exact.
-
-
-
-///const pageHTML = generatePage(name, github);
-
-///fs.writeFile('./index.html', pageHTML, err => {
-///  if (err) throw err;
-
-///  console.log('Portfolio complete! Check out index.html to see the output!');
-///});
-
-
-/* above, instead of hard-wiring the text into 
-the terminal output, we interpolated the text using 
-variables passed into the function that 
-created the output */
-
-
-
-
-/*
-9.1 Lesson - JS CODE
-const profileDataArgs = process.argv.slice(2);
-
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+  ]);
 };
 
-printProfileData(profileDataArgs);
-*/
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
 
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
 
-/* 9.2 Lesson
-Goal of this second lesson is to generate a file
-*/
-
-
-
-
-
-
-
-/* notes to self
-learn the difference between variables and methods - it's important to know when something is a method or functon versus a value of those
-*/
-
-/* notes on Node.JS
-We can't use the Web API because we aren't running the application in the browser.
-This prevents us from using the DOM or DOM methods to create HTML elements. 
-Without the DOM, how do we create the HTML?
-
-One solution is to create a function that returns HTML as a string. 
-Remember, HTML is just text set up in a structured format using markup. 
-This solution would generate the HTML, which we could then later output as an HTML file. 
-To start, let's have the function return a string, and then at the end of this step we'll modify the string into HTML
-*/
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+    // will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
